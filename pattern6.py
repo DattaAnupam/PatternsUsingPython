2b16ddd709a2f9a8df985714df966dc802ccd400
DattaAnupam <aanupamdatta.1995@gmail.com>
2020-06-19 20:03:06 +0530

    added docstring for pattern6.py file

--- pattern6.py
+++ pattern6.py
@@ -1,4 +1,10 @@
 def pattern6(n):
+	"""
+	chr(): method is used to convert to char data type.
+	ASCII value of A: 65
+	As col will stop before going to 0, we are starting 1 ascii value earlier.
+	if for consideration, n = 6, chr(6+64) = chr(70) = 'F'
+	"""
 	for row in range(n, 0, -1):
 		for col in range(0, row):
 			print(chr(row+64), end="")
